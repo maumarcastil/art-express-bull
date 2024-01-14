0852d179f905712eeb4dc067b3b0de75d1eed911
Mauricio Martinez <maumarcastil@gmail.com>
2024-01-14 02:25:50 -0500

    feat: Add configuration and load environment variables

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,6 +1,13 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const {
-  PORT
-} = process.env
+export const config = {
+  node_env: process.env.NODE_ENV ?? 'development',
+  port: process.env.PORT ?? 3000,
+  redis: {
+    host: process.env.REDIS_HOST ?? 'localhost',
+    port: process.env.REDIS_PORT ?? 6379,
+    db: process.env.REDIS_DB ?? 0,
+    password: process.env.REDIS_PASSWORD ?? ''
+  }
+}
