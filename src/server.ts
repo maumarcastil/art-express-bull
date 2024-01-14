4ae977043d9f84f519fe3be4c38094519728ef87
Mauricio Martinez <maumarcastil@gmail.com>
2024-01-14 02:26:51 -0500

    feat: Add arenaConfig

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
+import Bull from 'bull'
+import Arena from 'bull-arena'
 import { type Express } from 'express'
 
+import { queues } from './queues'
+
 export const initExpressServer = (app: Express, port: number): void => {
+  const arenaConfig = Arena({
+    Bull,
+    queues
+  }, {
+    basePath: '/arena',
+    disableListen: true
+  })
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  app.use('/', arenaConfig)
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
   })
